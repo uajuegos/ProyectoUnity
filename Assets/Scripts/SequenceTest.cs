e45a58e8dd47038726dcbcec5dd9c8f96d40f3e3
Raúl Serrano Gómez <raulserranodev@gmail.com>
2019-02-19 15:38:39 +0100

    19.02. Arreglados errores

---
+++ Assets/Scripts/SequenceTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SequenceTest : MonoBehaviour {
+
+    private KeyCode[] sequence = new KeyCode[] { KeyCode.P, KeyCode.A, KeyCode.S, KeyCode.S };
+
+    private int sequenceIndex;
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(sequence[sequenceIndex]))
+        {
+            if (++sequenceIndex == sequence.Length)
+            {
+                sequenceIndex = 0;
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
+        }
+        else if (Input.anyKeyDown)
+        {
+            sequenceIndex = 0;
+        }
+	}
+}
