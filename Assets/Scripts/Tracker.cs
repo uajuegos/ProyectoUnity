358ff395a11a746061e7861e4e512000e11c7ac2
AlvaroErrepeBass <phiram91@gmail.com>
2019-04-04 17:43:54 +0200

    Empezamos la hebra de flush

--- Assets/Scripts/Tracker.cs
+++ Assets/Scripts/Tracker.cs
@@ -1,25 +1,66 @@
 ﻿using UajTracker;
 using System;
 using System.Collections;
-public class Tracker{
+using System.Collections.Generic;
+using System.Threading;
+
+public class Tracker {
     
     private static Tracker instance = null;
 
     public Alternative Alternative;
-    private Queue cola = new Queue<Event>();
+    private Queue<Event> cola = new Queue<Event>();
+    private Queue<Event> pendientes = new Queue<Event>();
+
 
     public bool AddEvnt(Event e)
     {
-        cola.Enqueue(e);
+        if (!flushing)
+        {
+            lock (cola)
+            {
+                if (pendientes.Count > 0)
+                {
+
+                    cola = new Queue<Event>(pendientes);
+                    pendientes.Clear();
+                }
+                cola.Enqueue(e);
+            }
+        }
+        else
+            pendientes.Enqueue(e);
+
         return true;
     }
-
+    readonly object flushLockObject = new object();
+    bool exit;
+    bool flushing;
     public void Flush()
     {
         //disco o servidor
-        p.Flush();
+        
+            while (!exit)
+            {
+                lock (cola)
+                {
+                    flushing = true;
+                    ProcessQueue();
+                    flushing = false;
+                }
+            }
     }
     FileStyleUriParser p;
+
+    private void ProcessQueue() {
+        while (cola.Count > 0) {
+            Event e = cola.Dequeue();
+            //serializer.Serialize(e)
+        }
+
+        //persintance.Write()
+
+    }
     private Tracker()
     {
 
